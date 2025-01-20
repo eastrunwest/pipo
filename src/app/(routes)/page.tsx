4cd75168d41cee9389bae5dbc3a7bd63c3bb7f92
Charlie <cailiveinusa@gmail.com>
2025-01-21 07:36:14 +0800

    update desktop and mobile Nav UI

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,28 +1,57 @@
-import {auth, signIn, signOut} from "@/auth";
+import { auth, signIn, signOut } from "@/auth";
 import Preloader from "@/components/Preloader";
 import UserHome from "@/components/UserHome";
-import {Suspense} from "react";
+import PostsGrid from "@/components/PostsGrid";
+import { prisma } from "@/db";
+import SearchForm from "@/components/SearchForm";
+import SearchResults from "@/components/SearchResults";
+import { Suspense } from "react";
 
-export default async function Home() {
+export default async function Home({
+  searchParams: { query },
+}: {
+  searchParams: { query: string },
+}) {
   const session = await auth();
+  const posts = await prisma.post.findMany({
+    orderBy: { createdAt: 'desc' },
+    take: 100,
+  });
   return (
-    <div className="">
-      {session && (
-        <Suspense fallback={<Preloader />}>
-          <UserHome session={session} />
-        </Suspense>
-      )}
-      {!session && (
-        <form action={async () => {
-          'use server';
-          await signIn('google');
-        }}>
-          <button
-            className="border px-4 py-2 bg-ig-red text-white rounded-lg"
-            type="submit">Login with google
-          </button>
-        </form>
-      )}
+    <div>
+      <div className="w-full">
+        <div className="max-w-md mx-auto fixed top-0 left-0 right-0 z-10 p-4">
+          <SearchForm />
+        </div>
+        {typeof query !== 'undefined' && (
+          <Suspense fallback={<Preloader />}>
+            <SearchResults query={query} />
+          </Suspense>
+        )}
+
+      </div>
+      <div className="mt-12">
+        <PostsGrid posts={posts} />
+      </div>
     </div>
+
+    // <div className="">
+    //   {session && (
+    //     <Suspense fallback={<Preloader />}>
+    //       <UserHome session={session} />
+    //     </Suspense>
+    //   )}
+    //   {!session && (
+    //     <form action={async () => {
+    //       'use server';
+    //       await signIn('google');
+    //     }}>
+    //       <button
+    //         className="border px-4 py-2 bg-ig-red text-white rounded-lg"
+    //         type="submit">Login with google
+    //       </button>
+    //     </form>
+    //   )}
+    // </div>
   );
 }
--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -6,21 +6,21 @@ export default function DesktopNav() {
     <div className="hidden md:block px-4 pb-4 w-48 shadow-md shadow-gray-400 dark:shadow-gray-600">
       <div className="top-4 sticky">
         <img className="dark:invert"
-             src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png"
+             src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXNjYWxlIj48cGF0aCBkPSJtMTYgMTYgMy04IDMgOGMtLjg3LjY1LTEuOTIgMS0zIDFzLTIuMTMtLjM1LTMtMVoiLz48cGF0aCBkPSJtMiAxNiAzLTggMyA4Yy0uODcuNjUtMS45MiAxLTMgMXMtMi4xMy0uMzUtMy0xWiIvPjxwYXRoIGQ9Ik03IDIxaDEwIi8+PHBhdGggZD0iTTEyIDN2MTgiLz48cGF0aCBkPSJNMyA3aDJjMiAwIDUtMSA3LTIgMiAxIDUgMiA3IDJoMiIvPjwvc3ZnPg=="
              alt=""/>
         <div className="ml-1 inline-flex flex-col gap-6 mt-8 *:flex *:items-center *:gap-2">
           <Link href={'/'}>
             <HomeIcon/>
             Home
           </Link>
-          <Link href={'/search'}>
+          {/* <Link href={'/search'}>
             <SearchIcon/>
             Search
           </Link>
           <Link href={'/browse'}>
             <LayoutGridIcon/>
             Browse
-          </Link>
+          </Link> */}
           <Link href={'/profile'}>
             <UserIcon/>
             Profile
